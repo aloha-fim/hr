cda9e7ef23c85d2b9a79f7f73ffaf63849aef9d3
aloha-fim <frederec.im@gmail.com>
2023-12-11 22:32:38 +0800

    correct config for langchain

--- langchain/projects/multidocs/app2.py
+++ langchain/projects/multidocs/app2.py
@@ -11,4 +11,21 @@ from langchain.document_loaders import PyPDFLoader
 # chat with your data by wrap LLM
 from langchain.chains.question_answering import load_qa_chain
 
+# activate the env function
+# can use decouple library to import config
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# OpenAI API configs for Langchain
+# Per Langchain docs
+llm_model = "gpt-4-1106-preview"
+# Per Langchain docs
+# Temperature config on how creative GPT will output
+llm = ChatOpenAI(temperature=0.0, model=llm_model)
+
+print(llm.predict("What is the weather in WA DC"))
+
+
+
+
 
